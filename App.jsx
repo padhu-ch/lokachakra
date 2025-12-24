12cbb0f2ecb89e9cb2d119c4ce3aeef2c9e39d72
padhu-ch <padhuch09@gmail.com>
2025-12-24 20:50:34 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { CartProvider } from './context/CartContext';
+import Header from './components/Header';
+import Home from './pages/Home';
+import ProductListing from './pages/ProductListing';
+import ProductDetails from './pages/ProductDetails';
+import Cart from './pages/Cart';
+import Checkout from './pages/Checkout';
+
+function App() {
+  return (
+    <Router>
+      <CartProvider>
+        <div className="app">
+          <Header />
+          <main className="main-content">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/products" element={<ProductListing />} />
+              <Route path="/products/:id" element={<ProductDetails />} />
+              <Route path="/cart" element={<Cart />} />
+              <Route path="/checkout" element={<Checkout />} />
+            </Routes>
+          </main>
+
+          <footer className="footer">
+            <div className="container">
+              <p>&copy; 2024 Lumina E-Commerce. All rights reserved.</p>
+            </div>
+          </footer>
+        </div>
+      </CartProvider>
+    </Router>
+  );
+}
+
+export default App;
